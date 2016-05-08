b3c5035547a43bf7a4f0cc0fd63d43f93671cfd5
HamsterBalls <hamsterballswastaken@gmail.com>
2016-05-07 22:50:54 -0300

    first version
    esto y aquello y eso otro

---
+++ New Unity Project/Assets/Scripts/cameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraController : MonoBehaviour {
+
+	public GameObject player;
+	//so we reference the player on the inspector
+	private Vector3 offset;
+	//so we can calculate distance between player and camera
+	void Start ()
+	{
+		offset = transform.position - player.transform.position;
+	}
+	
+	void LateUpdate ()
+		//on lateUpdate because here goes the animation thingies ¿?
+	{
+		transform.position = player.transform.position + offset;
+	}
+}
